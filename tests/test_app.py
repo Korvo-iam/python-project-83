31d52762df32683c895919a58716909bccef8918
Korvo-iam <emilaubakirov@gmail.com>
2025-07-16 17:24:00 +0500

    172316072025

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,4 +4,5 @@ def test_index():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Анализатор страниц" in response.data
+    html_response = response.data.decode('utf-8')
+    assert "Анализатор страниц" in html_response
