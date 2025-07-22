f03c29d9a18312f8fe63d7a0a75300f652461725
Korvo-iam <emilaubakirov@gmail.com>
2025-07-22 17:36:30 +0500

    173622072025

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -6,7 +6,6 @@ from urllib.parse import urlparse
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
-conn = psycopg2.connect(DATABASE_URL)
 
 def get_connection():
     return psycopg2.connect(DATABASE_URL)
