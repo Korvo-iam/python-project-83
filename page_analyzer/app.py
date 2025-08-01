863678e4af264afe35c44f3ad74e8536f603bea6
Korvo-iam <emilaubakirov@gmail.com>
2025-08-01 15:57:26 +0500

    155701082025

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -43,9 +43,9 @@ def index_general():
                 flash(message, 'info')
                 return redirect(url_for('index_url_id', id=result))
             flash(message, 'danger')
-            #return redirect(url_for('index_general', url=url_orig))
+            return redirect(url_for('index_general', url=url_orig))
             #return make_response(render_template('index.html', url_value=url_orig), 422)
-            return make_response(redirect(url_for('index_urls')), 200)
+            #return make_response(redirect(url_for('index_urls')), 200)
 
 
 @app.route('/urls', methods = ['GET'])
