daa82b23bcf95dda0d62d29972da96591d35273d
Korvo-iam <emilaubakirov@gmail.com>
2025-08-01 18:00:30 +0500

    180001082025

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -43,28 +43,19 @@ def index_general():
                 flash(message, 'info')
                 return redirect(url_for('index_url_id', id=result))
             #experminet
-            flash(message, 'danger')
-            return redirect(url_for('index_urls', error=message))
             #norm
-            #flash(message, 'danger')
+            flash(message, 'danger')
             #return make_response(redirect(url_for('index_urls')), 422)
-            #return redirect(url_for('index_urls'))
+            return redirect(url_for('index_urls'))
             #return make_response(render_template('index.html', url_value=url_orig), 422)
             #return make_response(redirect(url_for('index_urls', url= url_orig)), 422)
 
+
+
 @app.route('/urls', methods=['GET'])
 def index_urls():
-    error_message = request.args.get('error')
-    if error_message:
-        # отрисовать index.html с ошибкой
-        return render_template('index.html', error=error_message, url_value='')
-    urls = sql_commands.return_urls()
-    return render_template('urls/index.html', urls=urls)
-
-#@app.route('/urls', methods=['GET'])
-#def index_urls():
-#    urls = sql_commands.return_urls()
-#    return render_template('urls/index.html', urls=urls)
+   urls = sql_commands.return_urls()
+   return render_template('urls/index.html', urls=urls)
 
 @app.route('/urls/<int:id>', methods = ['GET'])
 def index_url_id(id):
@@ -93,14 +84,3 @@ def check_urls(id):
     except requests.RequestException:
         flash('Произошла ошибка при проверке', 'danger')
         return redirect(url_for('index_url_id', id=id))
-
-@app.route('/test-db')
-def test_db():
-    try:
-        with sql_commands.get_connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute('SELECT 1;')
-                result = cur.fetchone()
-        return f'Подключение к БД успешно! Результат запроса: {result[0]}'
-    except Exception as e:
-        return f'Ошибка подключения к БД: {e}'
