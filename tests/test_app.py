85588b51d4acefeb37f2b129e757f831fd548890
Korvo-iam <emilaubakirov@gmail.com>
2025-08-04 19:36:37 +0500

    193604082025

--- tests/test_app.py
+++ tests/test_app.py
@@ -19,7 +19,7 @@ def test_add_url(monkeypatch):
     client = app.test_client()
     monkeypatch.setattr('page_analyzer.sql_commands.check_if_in_db', lambda url: None)  # noqa: E501
     monkeypatch.setattr('page_analyzer.sql_commands.add_in_db', lambda url: 999)
-    monkeypatch.setattr('page_analyzer.sql_commands.return_url_checks', lambda id:(mock_url,[]))  # noqa: E501
+    monkeypatch.setattr('page_analyzer.sql_commands.return_url_checks', lambda id: (mock_url, []))  # noqa: E501
     response = client.post('/', data={'url': 'https://example.com'}, follow_redirects=True)  # noqa: E501
     assert response.status_code == 200
     assert "Страница успешно добавлена" in response.data.decode('utf-8')
@@ -27,7 +27,8 @@ def test_add_url(monkeypatch):
 
 def test_add_incorrect_url():
     client = app.test_client()
-    response = client.post('/', data={'url': 'not a url'}, follow_redirects=True)
+    response = client.post('/', data={'url': 'not a url'}, follow_redirects=True)  # noqa: E501
+    assert response.status_code == 200
     assert response.status_code == 200
     assert "Некорректный URL" in response.data.decode('utf-8')
 
@@ -35,7 +36,8 @@ def test_add_incorrect_url():
 def test_urls_list(monkeypatch):
     client = app.test_client()
     fake_urls = [(1, 'https://example.com', '2025-07-27', 200)]
-    monkeypatch.setattr('page_analyzer.sql_commands.return_urls', lambda: fake_urls)
+    monkeypatch.setattr('page_analyzer.sql_commands.return_urls', lambda: fake_urls)  # noqa: E501
+    assert response.status_code == 200
     response = client.get('/urls')
     assert response.status_code == 200
     html = response.data.decode('utf-8')
@@ -49,7 +51,8 @@ def test_url_detail(monkeypatch):
         def __init__(self, id):
             self.id = id
     mock_url = url(id=69)
-    mock_checks = [(1, 200, 'Анализатор страниц', 'Page analyzer', 'h1teg', '2025-07-28')]
+    mock_checks = [(1, 200, 'Анализатор страниц', 'Page analyzer', 'h1teg', '2025-07-28')]  # noqa: E501
+    assert response.status_code == 200
     monkeypatch.setattr('page_analyzer.sql_commands.return_url_checks', lambda id: (mock_url, mock_checks))  # noqa: E501
     response = client.get('/urls/69')
     assert response.status_code == 200
